6f43b79efc7b5ca4e754e7e6629464e1b3a63f26
Vaska Lazarova <vaska_lazarova@live.com>
2021-09-22 21:25:04 +0200

    added api calls

--- ad-catalog/src/main/java/com/finki/emt/adcatalog/xport/rest/AdResource.java
+++ ad-catalog/src/main/java/com/finki/emt/adcatalog/xport/rest/AdResource.java
@@ -1,12 +1,12 @@
 package com.finki.emt.adcatalog.xport.rest;
 
 import com.finki.emt.adcatalog.domain.models.Ad;
+import com.finki.emt.adcatalog.domain.models.AdId;
 import com.finki.emt.adcatalog.services.AdService;
+import com.finki.emt.adcatalog.services.form.AdForm;
 import lombok.AllArgsConstructor;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +19,21 @@ public class AdResource {
     @GetMapping()
     public List<Ad> getAll() {
         return adService.getAll();
+    }
+
+    @PostMapping("/create")
+    public Ad createAd(@RequestBody AdForm request) {
+        return adService.createAd(request);
+    }
+
+    @GetMapping("/{id}")
+    public Ad getById(@PathVariable("id") String id) {
+        return adService.findById(AdId.of(id));
+    }
+
+    @PutMapping("/{adId}/subscribe")
+    public Ad subscriptionItemCreated(@PathVariable("adId") String id) {
+        return adService.subscriptionItemCreated(AdId.of(id));
     }
 
 }
